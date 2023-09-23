e3610751b99c2dd5a42edc524f8e9354561a88d3
AliHejazi1382 <mrhjz08@gmail.com>
2023-09-23 10:40:53 +0330

    Scaffold pages added from previous project

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 void main() {
   runApp(const MyApp());
@@ -19,21 +20,81 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+class AuthScaffold extends StatelessWidget {
+  const AuthScaffold({super.key, required this.child});
 
-
-class MyHomePage extends StatelessWidget {
-
-  const MyHomePage({super.key});
+  final Widget child;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      body: child,
+    );
+  }
+}
 
-          title: const Text('Home'),
+class HomeScaffold extends StatelessWidget {
+  const HomeScaffold({super.key, required this.child});
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+        body: child,
+        bottomNavigationBar: BottomNavigationBar(
+          items: const [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.settings), label: 'Settings'),
+            BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
+          ],
+          currentIndex: _handleCurrentIndex(context),
+          onTap: (index) => _onItemTap(index, context),
         ),
-        body: const Center()
+    );
+  }
+
+  int _handleCurrentIndex(BuildContext context) {
+    final String location = GoRouterState.of(context).uri.toString();
+    if (location.startsWith('/home')) {
+      return 0;
+    }
+    if (location.startsWith('/settings')) {
+      return 1;
+    }
+    if (location.startsWith('/profile')) {
+      return 2;
+    }
+
+    return 0;
+  }
+
+  void _onItemTap(int index, BuildContext context) {
+    switch (index) {
+      case 0:
+        GoRouter.of(context).go('/home');
+        break;
+      case 1:
+        GoRouter.of(context).go('/settings');
+        break;
+      case 2:
+        GoRouter.of(context).go('/profile');
+        break;
+    }
+  }
+}
+
+class DetailScaffold extends StatelessWidget {
+  const DetailScaffold({super.key, required this.child});
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: child,
     );
   }
 }
